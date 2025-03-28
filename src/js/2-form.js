400f37b809be28007172f5e8aa2b77e713a11507
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-03-29 00:36:43 +0200

    finish

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,38 @@
-console.log('Hello');
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email || '';
+  form.elements.message.value = formData.message || '';
+}
+
+const updateStorage = () => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  updateStorage();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  formData = { email: '', message: '' };
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+});
